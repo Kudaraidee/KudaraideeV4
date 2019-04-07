1d130dddd028e09341392e8308b8bb6aab0207a7
Decho Kocharin <sourcedecho@outlook.com>
2019-04-07 21:43:55 +0700

    Add verushash miner

---
+++ Miners/CcminerMonkins1010.ps1
@@ -0,0 +1,27 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+
+$Path = ".\Bin\NVIDIA-CcminerMonkins1010\ccminer.exe"
+$Uri = "https://github.com/Kudaraidee/CcminerMonkins1010/releases/download/v3.3/CCminer.Verushash2.v3.3.zip"
+
+$Commands = [PSCustomObject]@{
+    "verushash"   = " -a verus " #verus
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    $Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "-b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * (1 - $Fee)} #q substract 1% devfee
+        API       = "ccminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri
+        User = $Pools.($Algo).User
+        Host = $Pools.($Algo).Host
+        Coin = $Pools.($Algo).Coin
+    }
+}

50bd0e8de056c3846eed4048d10f341aacbe3344
Decho Kocharin <sourcedecho@outlook.com>
2019-04-11 12:19:13 +0700

    Update trex.ps1

--- OptionalMiners/trex.ps1
+++ OptionalMiners/trex.ps1
@@ -4,31 +4,31 @@ $Path = ".\Bin\NVIDIA-Trex\t-rex.exe"
 $Uri = "https://github.com/trexminer/T-Rex/releases/download/0.9.2/t-rex-0.9.2-win-cuda10.0.zip"
 
 $Commands = [PSCustomObject]@{
-"astralhash" = "" #Astralhash(GlobalToken)
-"balloon" = "" #Balloon
-"bcd" = "" #Bcd
-"bitcore" = "" #Bitcore
-"c11" = "" #C11
-"dedal" = "" #Dedal
-"geek"= "" #Geekcash
-"hmq1725" = "" #hmq1725
-"hsr" = "" #Hsr(Testing)
-"jeonghash" = "" #Jeonghash(GlobalToken)
-"lyra2z" = "" #Lyra2z 
-"padihash" = "" #Padihash(GlobalToken)
-"pawelhash " = "" #Pawelhash(GlobalToken)
-"polytimos" = "" #Poly
-"sha256t" = "" #Sha256t
-"skunk" = "" #Skunk
-"sonoa" = "" #SonoA
-"tribus" = "" #Tribus
-"x16r" = "" #X16r
-"x16rt" = "" #X16RT (GIN)
-"veil" = "" #X16RT (VEIL)
-"x16s" = "" #X16s
-"x17" = "" #X17
-"x21s" = "" #X21S
-"x22i" = "" #X22I
+"astralhash" = " -a astralhash" #Astralhash(GlobalToken)
+"balloon" = " -a balloon" #Balloon
+"bcd" = " -a bcd" #Bcd
+"bitcore" = " -a bitcore" #Bitcore
+"c11" = " -a c11" #C11
+"dedal" = " -a dedal" #Dedal
+"geek"= " -a geek" #Geekcash
+"hmq1725" = " -a hmq1725" #hmq1725
+"hsr" = " -a hsr" #Hsr(Testing)
+"jeonghash" = " -a jeonghash" #Jeonghash(GlobalToken)
+"lyra2z" = " -a lyra2z" #Lyra2z 
+"padihash" = " -a padihash" #Padihash(GlobalToken)
+"pawelhash " = " -a pawelhash" #Pawelhash(GlobalToken)
+"polytimos" = " -a polytimos" #Poly
+"sha256t" = " -a sha256t" #Sha256t
+"skunk" = " -a skunk" #Skunk
+"sonoa" = " -a sonoa" #SonoA
+"tribus" = " -a tribus" #Tribus
+"x16r" = " -a x16r" #X16r
+"x16rt" = " -a x16rt" #X16RT (GIN)
+"veil" = " -a x16rt" #X16RT (VEIL)
+"x16s" = " -a x16s" #X16s
+"x17" = " -a x17" #X17
+"x21s" = " -a x21s" #X21S
+"x22i" = " -a x22i" #X22I
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -37,7 +37,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = " -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = " -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .99} # substract 1% devfee
         API = "ccminer"
         Port = $Variables.NVIDIAMinerAPITCPPort

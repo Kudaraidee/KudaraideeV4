b8011458fd0331fb1941758543892c7f96c7af8a
Decho Kocharin <sourcedecho@outlook.com>
2019-04-17 13:10:45 +0700

    Update WildRigMulti.ps1

--- OptionalMiners/WildRigMulti.ps1
+++ OptionalMiners/WildRigMulti.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
 $Path = '.\Bin\AMD-WildRigMulti\wildrig.exe'
-$Uri = 'https://github.com/andru-kun/wildrig-multi/releases/download/0.15.4p18/wildrig-multi-windows-0.15.4-preview18.7z'
+$Uri = 'https://github.com/andru-kun/wildrig-multi/releases/download/0.15.5/wildrig-multi-windows-0.15.5.7z'
 
 $Commands = [PSCustomObject]@{
     "bcd"           = " --algo bcd" #BitcoinDiamond
@@ -12,7 +12,8 @@ $Commands = [PSCustomObject]@{
     "geek"          = " --algo geek" #GeekCash
     "hex"           = " --algo hex" #XDNA
     "hmq1725"       = " --algo hmq1725" #Hmq1725
-    "lyra2c0ban"    = " --algo lyra2vc0ban" #lyra2c0ban
+    "honeycomb"		= " --algo honeycomb" #Honeycomb [BeeNode]
+	"lyra2c0ban"    = " --algo lyra2vc0ban" #lyra2c0ban
     "lyra2v3"       = " --algo lyra2v3"
     "phi"           = " --algo phi" #Phi
     "polytimos"     = " --algo polytimos"
@@ -25,7 +26,7 @@ $Commands = [PSCustomObject]@{
     #"timetravel10" = " --algo timetravel10" #Bitcore
     "tribus"        = " --algo tribus" #Tribus
     "x16r"          = " --algo x16r" #x16r
-    "x16rt"         = " --algo x16rt"
+    "x16rt"         = " --algo x16rt" #GIN x16rt
     "x16s"          = " --algo x16s" #x16s
     "x17"           = " --algo x17" #x17
     "x21s"          = " --algo x21s" #x21s
@@ -35,6 +36,8 @@ $Commands = [PSCustomObject]@{
     "astralhash"    = " --algo glt-astralhash" #Astralhash
     "padihash"      = " --algo glt-padihash" #Padihash
     "rainforest"    = ",d=100 --algo rainforest" #rainforest
+	"veil"         = " --algo x16rt" #Veil x16rt
+    
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
